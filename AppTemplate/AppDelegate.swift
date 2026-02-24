ddc5c55b8c8467c5f765e3ede955e2fdfe2f7323
Developer <Lepiozdyx@gmail.com>
2026-02-24 12:51:25 -0300

    up vers, remove pn/nw/wv

--- AppTemplate/AppDelegate.swift
+++ AppTemplate/AppDelegate.swift
@@ -9,39 +9,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         self.application = application
-//        onGameStart()
-//        return true
-        
-        showLoadingScreen()
-        initApp()
-        
-        if let notification = launchOptions?[.remoteNotification] as? [AnyHashable: Any] {
-            onPushRecieved(content: notification)
-        }
+        onGameStart()
         return true
+        
+//        showLoadingScreen()
+//        initApp()
+//        
+//        if let notification = launchOptions?[.remoteNotification] as? [AnyHashable: Any] {
+//            onPushRecieved(content: notification)
+//        }
+//        return true
     }
     
     func onGameStart()
     {
-        // Здесь происходит инициализация главного View
-        // InitialView нужно заменить на ваш главный View
         let contentView = CustomHostingController(rootView: MainTabView())
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = contentView
 
-        // Также, контроль ориентации происходит за счет класса OrientationHelper
-        // Ориентация задается за счет переменной orientaionMask
-        // Если нужно отключить автоповорот, то нужно установить isAutoRotationEnabled в false
         OrientationHelper.orientaionMask = UIInterfaceOrientationMask.portrait
         OrientationHelper.isAutoRotationEnabled = false
 
-        // Вся остальная логика приложения, которая должна быть выполнена до загрузки главного View
-        // должна быть выполнена здесь. Например, инициализация аудио
-        
-
-        ///////////////////////////////
-
-        // Показываем главный View
         window?.makeKeyAndVisible()
     }
 }
